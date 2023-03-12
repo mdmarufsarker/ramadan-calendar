0a2556d68d0bb47bb4781227a988a424cf179275
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2023-03-12 23:33:58 +0600

    Making responsive

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div>
-    <h1>Ramadan Calendar 2023</h1>
-    <hr />
-    <hr />
-    <div class="container">
-      <Rahmat />
-      <Magfirat />
-      <Najat />
+    <div>
+        <h1>Ramadan Calendar 2023</h1>
+        <hr />
+        <hr />
+        <div class="container">
+            <Rahmat />
+            <Magfirat />
+            <Najat />
+        </div>
+        <footer>
+            <h3>© 2022 Copyright | All rights reserved | Md. Maruf Sarker</h3>
+        </footer>
     </div>
-    <footer>
-      <h3>© 2022 Copyright | All rights reserved | Md. Maruf Sarker</h3>
-    </footer>
-  </div>
 </template>
 
 <script>
@@ -21,50 +21,85 @@ import Magfirat from "./components/Magfirat.vue";
 import Najat from "./components/Najat.vue";
 
 export default {
-  components: {
-    Rahmat,
-    Magfirat,
-    Najat,
-  },
-  data() {
-    return {};
-  },
+    components: {
+        Rahmat,
+        Magfirat,
+        Najat,
+    },
+    data() {
+        return {};
+    },
 };
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Itim&display=swap");
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 body {
-  font-family: "Itim", cursive;
-  background-color: dodgerblue;
+    font-family: "Itim", cursive;
+    background-color: dodgerblue;
 }
 h1 {
-  color: #fff;
-  text-align: center;
-  padding-top: 20px;
+    color: #fff;
+    text-align: center;
+    padding-top: 20px;
 }
 hr {
-  height: 2px;
-  width: 300px;
-  margin: 3px auto;
-  background-color: #000;
-  border: none;
+    height: 2px;
+    width: 300px;
+    margin: 3px auto;
+    background-color: #000;
+    border: none;
 }
 .container {
-  display: flex;
-  flex-direction: row;
-  margin-top: 10px;
-  justify-content: space-around;
-  flex-wrap: wrap;
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    justify-content: space-around;
+    flex-wrap: wrap;
 }
 h3 {
-  text-align: center;
-  font-weight: 400;
-  margin-bottom: 10px;
+    text-align: center;
+    font-weight: 400;
+    margin-bottom: 10px;
+}
+
+@media (min-width: 1921px) {
+    #app {
+        width: 1920px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+}
+
+@media (max-width: 1440px) {
+    .container {
+        width: 80%;
+        margin: auto;
+    }
+
+    footer {
+        font-size: 10px;
+        width: 80%;
+        margin: auto;
+    }
+}
+
+@media (max-width: 1024px) {
+    .container {
+        width: 90%;
+        margin: auto;
+    }
+    h1 {
+        font-size: 28px;
+    }
+    hr {
+        height: 1.3px;
+        width: 90%;
+    }
 }
 </style>
--- src/components/Rahmat.vue
+++ src/components/Rahmat.vue
@@ -1,112 +1,161 @@
 <template>
-  <div class="rahmat">
-    <h2>10 days of Rahmat</h2>
-    <div class="wrapper">
-      <div class="parent">
-        <span class="title">Count</span>
-        <div class="count child" v-for="count in counts" :key="count">
-          {{ count }}
+    <div class="rahmat">
+        <h2>10 days of Rahmat</h2>
+        <div class="wrapper">
+            <div class="parent">
+                <span class="title">Count</span>
+                <div class="count child" v-for="count in counts" :key="count">
+                    {{ count }}
+                </div>
+            </div>
+            <div class="parent">
+                <span class="title">Days</span>
+                <div class="day child" v-for="day in days" :key="day">
+                    {{ day }}
+                </div>
+            </div>
+            <div class="parent">
+                <span class="title">Seheri</span>
+                <div
+                    class="seheri child"
+                    v-for="seheri in seheri"
+                    :key="seheri"
+                >
+                    {{ seheri }}
+                </div>
+            </div>
+            <div class="parent">
+                <span class="title">Iftar</span>
+                <div class="iftar child" v-for="iftar in iftar" :key="iftar">
+                    {{ iftar }}
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="parent">
-        <span class="title">Days</span>
-        <div class="day child" v-for="day in days" :key="day">{{ day }}</div>
-      </div>
-      <div class="parent">
-        <span class="title">Seheri</span>
-        <div class="seheri child" v-for="seheri in seheri" :key="seheri">
-          {{ seheri }}
-        </div>
-      </div>
-      <div class="parent">
-        <span class="title">Iftar</span>
-        <div class="iftar child" v-for="iftar in iftar" :key="iftar">
-          {{ iftar }}
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      counts: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      days: [
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ],
-      seheri: [
-        "04:39",
-        "04:38",
-        "04:37",
-        "04:36",
-        "04:35",
-        "04:34",
-        "04:32",
-        "04:31",
-        "04:30",
-        "04:29",
-      ],
-      iftar: [
-        "06:14",
-        "06:14",
-        "06:14",
-        "06:15",
-        "06:15",
-        "06:16",
-        "06:16",
-        "06:17",
-        "06:17",
-        "06:18",
-      ],
-    };
-  },
+    data() {
+        return {
+            counts: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            days: [
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+            ],
+            seheri: [
+                "04:39",
+                "04:38",
+                "04:37",
+                "04:36",
+                "04:35",
+                "04:34",
+                "04:32",
+                "04:31",
+                "04:30",
+                "04:29",
+            ],
+            iftar: [
+                "06:14",
+                "06:14",
+                "06:14",
+                "06:15",
+                "06:15",
+                "06:16",
+                "06:16",
+                "06:17",
+                "06:17",
+                "06:18",
+            ],
+        };
+    },
 };
 </script>
 
 <style>
-.title{
-  display: flex;
-  justify-content: center;
-  padding: 0 10px;
-  font-size: 1.4rem;
-  font-weight: 600;
-  text-decoration: underline;
+.title {
+    display: flex;
+    justify-content: center;
+    padding: 0 10px;
+    font-size: 1.4rem;
+    font-weight: 600;
+    text-decoration: underline;
 }
 .rahmat {
-  width: 400px;
-  background-color: cyan;
-  margin: 20px;
-  border-radius: 10px;
+    width: 400px;
+    background-color: cyan;
+    margin: 20px;
+    border-radius: 10px;
+    text-align: center;
+    padding: 15px auto;
 }
 .rahmat h2 {
-  font-size: 25px;
-  text-align: center;
-  padding: 10px 0 5px 0;
-  border-bottom: 10px solid dodgerblue;
+    font-size: 25px;
+    text-align: center;
+    padding: 10px 0 5px 0;
+    border-bottom: 10px solid dodgerblue;
 }
 .rahmat .wrapper {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 15px;
 }
 .wrapper .parent {
-  line-height: 2;
-  font-size: 20px;
+    line-height: 2;
+    font-size: 20px;
 }
 .wrapper .parent .child {
-  padding: 5px 10px;
+    padding: 5px 10px;
 }
-@media only screen and (max-width: 1300px) {
+
+@media (max-width: 1440px) {
+    .title {
+        font-size: 1.2rem;
+    }
+    .rahmat {
+        width: 350px;
+        margin: 15px 10px;
+    }
+
+    .rahmat h2 {
+        font-size: 20px;
+    }
+
+    .wrapper .parent {
+        line-height: 1.7;
+        font-size: 17px;
+    }
+}
+
+@media (max-width: 1388px) {
+    .title {
+        font-size: 1rem;
+    }
+    .rahmat {
+        width: 250px;
+        margin: 15px 10px;
+    }
+
+    .rahmat h2 {
+        font-size: 16px;
+    }
+
+    .wrapper .parent {
+        line-height: 1.2;
+        font-size: 13px;
+    }
+
+    .wrapper .parent .child {
+        padding: 5px 2px;
+    }
 }
 </style>
