037908b63b26f610de1867e38d6de361d10b1306
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2024-06-13 22:25:35 +0600

    Add markup

---
+++ src/pages/ComingSoon/ComingSoon.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="coming-soon-page-container">
+        <div class="coming-soon-nav">
+            <div class="nav-left-part"><p>ramadan calender</p></div>
+        </div>
+        <div class="coming-soon-hero-section-container">
+            <div class="hero-text-container"><h2>Ramadan 2025</h2>
+                <p></p></div>
+            <div class="coming-soon-time-container"></div>
+        </div>
+    </div>
+</template>
+<script> export default {
+    components: {}, data() {
+        return {};
+    },
+}; </script>
+<style></style>
