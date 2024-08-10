1df0f7cc1724a56a11614bf230e5122980b20f73
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2024-08-10 21:59:52 +0600

    Add media queries

--- src/pages/ComingSoon/ComingSoon.vue
+++ src/pages/ComingSoon/ComingSoon.vue
@@ -29,7 +29,7 @@ setInterval(() => {
 
 <template>
     <div class="coming-soon-page-container">
-        <div class="main-container">
+        <div class="coming-soon-main-container">
             <!-- <div class="coming-soon-nav">
                 <div class="nav-left-part"><p>Ramadan Calendar</p></div>
             </div> -->
@@ -37,7 +37,7 @@ setInterval(() => {
                 <div class="hero-text-container">
                     <h2>Ramadan 2025</h2>
                     <p>
-                        Assalam o alaikum, dear muslims! how are you? ramadan is
+                        Assalamu Alaikum, Dear muslims! How are you? Ramadan is
                         coming again in
                     </p>
                 </div>
@@ -72,6 +72,12 @@ setInterval(() => {
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@300..900&display=swap");
 
+.coming-soon-main-container {
+    min-height: 100vh;
+    width: 60%;
+    margin: auto;
+}
+
 .coming-soon-page-container {
     min-height: 100vh;
     min-width: 100%;
@@ -83,7 +89,7 @@ setInterval(() => {
     padding: 30px 0%;
 }
 
-.coming-soon-hero-section-container{
+.coming-soon-hero-section-container {
     height: 100vh;
     display: flex;
     flex-direction: column;
@@ -95,6 +101,7 @@ setInterval(() => {
     font-size: 100px;
     color: #fff;
     text-align: center;
+    text-shadow: 2px 2px #222;
 }
 
 .coming-soon-hero-section-container .hero-text-container p {
@@ -103,7 +110,7 @@ setInterval(() => {
     text-align: center;
     margin: 20px auto 40px auto;
     width: 90%;
-
+    text-shadow: 2px 2px 5px #222;
 }
 
 .coming-soon-time-container {
@@ -124,9 +131,127 @@ setInterval(() => {
 
 .coming-soon-time h2 {
     font-size: 100px;
+    text-shadow: 2px 2px #222;
 }
 
 .coming-soon-time p {
     font-size: 30px;
+    text-shadow: 2px 2px 5px #222;
+}
+
+/* Media Queries */
+@media (max-width: 1440px) {
+    .coming-soon-hero-section-container .hero-text-container h2 {
+        font-size: 80px;
+    }
+
+    .coming-soon-hero-section-container .hero-text-container p {
+        font-size: 40px;
+    }
+
+    .coming-soon-time h2 {
+        font-size: 80px;
+    }
+
+    .coming-soon-time p {
+        font-size: 26px;
+    }
+
+    .coming-soon-time {
+        width: 90px;
+    }
+}
+
+@media (max-width: 1024px) {
+    .coming-soon-main-container{
+        width: 80%;
+    }
+    
+    .coming-soon-hero-section-container .hero-text-container h2 {
+        font-size: 70px;
+    }
+
+    .coming-soon-hero-section-container .hero-text-container p {
+        font-size: 34px;
+    }
+
+    .coming-soon-time h2 {
+        font-size: 70px;
+    }
+
+    .coming-soon-time p {
+        font-size: 22px;
+    }
+}
+
+@media (max-width: 768px) {
+    .coming-soon-hero-section-container .hero-text-container h2 {
+        font-size: 60px;
+    }
+
+    .coming-soon-hero-section-container .hero-text-container p {
+        font-size: 26px;
+        width: 100%;
+    }
+
+    .coming-soon-time h2 {
+        font-size: 60px;
+    }
+
+    .coming-soon-time p {
+        font-size: 20px;
+    }
+
+    .coming-soon-time {
+        width: 70px;
+    }
+}
+
+@media (max-width: 600px) {
+    .coming-soon-hero-section-container .hero-text-container h2 {
+        font-size: 50px;
+    }
+
+    .coming-soon-hero-section-container .hero-text-container p {
+        font-size: 22px;
+    }
+
+    .coming-soon-time h2 {
+        font-size: 50px;
+    }
+
+    .coming-soon-time p {
+        font-size: 18px;
+    }
+
+    .coming-soon-time {
+        width: 45px;
+    }
+}
+
+@media (max-width: 425px) {
+    .coming-soon-main-container{
+        width: 95%;
+    }
+    
+    .coming-soon-hero-section-container .hero-text-container h2 {
+        font-size: 40px;
+    }
+
+    .coming-soon-hero-section-container .hero-text-container p {
+        font-size: 18px;
+    }
+
+    .coming-soon-time h2 {
+        font-size: 40px;
+    }
+
+    .coming-soon-time p {
+        font-size: 16px;
+    }
+
+    .coming-soon-time {
+        width: 21px;
+    }
 }
 </style>
